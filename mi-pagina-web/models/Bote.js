5550ee8505c283c0c0995e50ae00064ab9e38403
cortesmmonse <cortes.martinez.maria.46@cbtis118.edu.mx>
2025-06-23 12:39:51 -0600

    Add files via upload

---
+++ mi-pagina-web/models/Bote.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const boteSchema = new mongoose.Schema({
+  tipo: { type: String, default: 'bote' },
+  ubicacion: String,
+  reciclaje: String
+}, { collection: 'PAEC' });
+
+module.exports = mongoose.model('Bote', boteSchema);
