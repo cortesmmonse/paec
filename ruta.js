4967492ea6cce77d08c6b2b5a3a0d94067cf4654
cortesmmonse <cortes.martinez.maria.46@cbtis118.edu.mx>
2025-06-24 11:06:13 -0600

    Delete ruta.js

--- ruta.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const rutaSchema = new mongoose.Schema({
-  zona: String,
-  hora: String,
-  descripcion: String
-});
-
-module.exports = mongoose.model('Ruta', rutaSchema);
