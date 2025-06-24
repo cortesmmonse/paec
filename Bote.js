5084521a00f945600365a2c56489291051a54ef0
cortesmmonse <cortes.martinez.maria.46@cbtis118.edu.mx>
2025-06-24 11:04:35 -0600

    Delete Bote.js

--- Bote.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const boteSchema = new mongoose.Schema({
-  tipo: { type: String, default: 'bote' },
-  ubicacion: String,
-  reciclaje: String
-}, { collection: 'PAEC' });
-
-module.exports = mongoose.model('Bote', boteSchema);
