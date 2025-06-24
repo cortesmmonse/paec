54d5603d095dd1f2a82683ca5998780a333ada56
cortesmmonse <cortes.martinez.maria.46@cbtis118.edu.mx>
2025-06-24 12:21:05 -0600

    Update Bote.js

--- models/Bote.js
+++ models/Bote.js
@@ -1,10 +1,8 @@
-// models/Bote.js
 const mongoose = require('mongoose');
 
 const boteSchema = new mongoose.Schema({
-  tipo: { type: String, default: 'bote' },
-  ubicacion: String,
-  reciclaje: String
+  tipo: String,          // Tipo de reciclaje del bote (plástico, etc.)
+  ubicacion: String      // Ubicación del bote
 }, { collection: 'PAEC' });
 
 module.exports = mongoose.model('Bote', boteSchema);
